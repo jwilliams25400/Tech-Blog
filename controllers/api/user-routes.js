973998fc65298adaeddeccf0593566946bba7419
Jemel Williams <jwilliams25400@yahoo.com>
2021-09-20 15:25:46 -0400

    completed user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -52,10 +52,11 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       // TODO: SET USERID userId IN REQUEST SESSION TO ID RETURNED FROM DATABASE
-
+      req.session.user_id = user_id
       // TODO: SET USERNAME username IN REQUEST SESSION TO USERNAME RETURNED FROM DATABASE
-
+      req.session.user_name = username
       // TODO: SET LOGGEDIN loggedIn TO TRUE IN REQUEST SESSION
+      req.session.loggedIn = true
 
       res.json({ user, message: 'You are now logged in!' });
     });
